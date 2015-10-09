64ad33bdf456d7110eb1fc69f36761bfe5bce16b
Reagan Thomas <reagan.m.thomas@gmail.com>
2015-10-09 12:43:09 -0500

    express config

---
+++ templates/server/config/express.js
@@ -0,0 +1,31 @@
+(function IIFE() {
+  'use strict';
+
+  var methodOverride = require('method-override');
+  var compression = require('compression');
+  var bodyParser = require('body-parser');
+  var express = require('express');
+  var path = require('path');
+  
+  var config = require('./environment');
+
+  module.exports = function(app) {
+    let env = app.get('env');
+
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+    app.use(compression());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
+    app.use('/docs', express.static('docs'));
+    app.set('appPath', path.join(config.root, 'docs'));
+
+    /* istanbul ignore next */
+    if(env === 'development' || env === 'test') {
+      app.use(require('connect-livereload')());
+      app.use(require('errorhandler')());
+    }
+  };
+})();
