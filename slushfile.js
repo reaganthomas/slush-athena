ef865feda29134238eb354652980976c1e91fedc
Reagan Thomas <reagan.m.thomas@gmail.com>
2015-10-10 13:42:02 -0500

    stub in sub tasks

--- slushfile.js
+++ slushfile.js
@@ -11,4 +11,12 @@
   gulp.task('default', function(done) {
     inquirer.prompt(scaffold.prompts, scaffold.template(done));
   });
+
+  gulp.task('model', function(done) {
+    inquirer.prompt(model.prompts, model.template(done));
+  });
+
+  gulp.task('controller', function(done) {
+
+  });
 })();
