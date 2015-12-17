75053e4bde98a0b2e4bbcba6aae6fe8c1f5563d8
reaganthomas <reaganthomas@users.noreply.github.com>
2015-12-17 15:42:21 -0600

    Update production.js

--- templates/server/config/environment/production.js
+++ templates/server/config/environment/production.js
@@ -2,9 +2,9 @@
   'use strict';
 
   module.exports = {
-    port: process.env.PORT<% if(mongoose) { %>,
+    port: process.env.PORT || throw new Error('PORT not defined')<% if(mongoose) { %>,
     mongo: {
-      uri: process.env.MONGO_URI,
+      uri: process.env.MONGO_URI || throw new Error('MONGO_URI not defined'),
       autoIndex: false
     }<% } %>
   };
